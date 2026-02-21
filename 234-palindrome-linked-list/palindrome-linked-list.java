0f37cd070435e30cdd4e50e39501739bcc074a43
YasharthChaudhary761 <yasharthchaudhary766@gmail.com>
2026-02-21 09:31:39 +0530

    Time: 4 ms (74.76%) | Memory: 96.6 MB (35.21%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,9 +9,215 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head)
-    {
-        if (head == null || head.next == null)
+    public boolean isPalindrome(ListNode head) {
+       // if(head==null || head.next==null) return true;
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+          fast=fast.next.next;
+          slow=slow.next;
+        }
+        ListNode newHead=reverseList(slow.next);
+        ListNode first=head;
+        ListNode second=newHead;
+        while(second!=null)
+        {
+            if(first.val!=second.val)
+            {
+                reverseList(newHead);
+                return false;
+            }
+            first=first.next;
+            second=second.next;
+        }
+        reverseList(newHead);
+        return true;
+    }
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode after=null;
+        while(curr!=null)
+        {
+            after=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=after;
+        }
+        return prev;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** if (head == null || head.next == null)
         {
             return head;
         }
@@ -45,5 +251,4 @@ class Solution {
         }
         reverseList(newHead);
         return true;
-    }
-}
+        */
