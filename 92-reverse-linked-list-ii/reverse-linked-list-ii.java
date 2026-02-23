d607673f1e6a1af1e080f69eae7d8078df436581
YasharthChaudhary761 <yasharthchaudhary766@gmail.com>
2026-02-23 16:06:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (48.02%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head==null || left==right) return head;
+        ListNode dummy=new ListNode(0,head);
+        
+        ListNode prev=dummy;
+        for(int i=0;i<left-1;++i)
+        {
+            prev=prev.next;
+        }
+
+        ListNode curr=prev.next;
+
+        for(int i=0;i<(right-left);++i)
+        {
+            ListNode NXT=curr.next;
+            curr.next=NXT.next;
+            NXT.next=prev.next;
+            prev.next=NXT;
+        }
+        return dummy.next;
+    }
+}
+
+
+
+/**if(head==null || head.next==null) return head;
+        ListNode temp=head;
+        ListNode leftHold=null;
+        ListNode rightHold=null;
+        
+        while(temp!=null)
+        {
+            if(temp.val==left)
+            {
+                leftHold=temp;
+            }
+            if(temp.val==right)
+            {
+                rightHold=temp;
+            }
+            temp=temp.next;
+        }
+        
+        ListNode curr=leftHold;
+        ListNode prev=null;
+        ListNode after=null;
+        ListNode rightend=rightHold.next;
+
+        while(curr.next!=null)
+        {
+            after=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=after;
+        }
+         while(prev.next!=null)
+        {
+            if(prev.next==null)
+            {
+                prev.next=rightend;
+            }
+        } 
+        head.next=prev;
+
+        return head; */
