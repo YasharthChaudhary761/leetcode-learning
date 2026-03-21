2f2e8ddb815eabfd2bba94d18f80c7ea8610b844
YasharthChaudhary761 <yasharthchaudhary766@gmail.com>
2026-03-21 22:18:02 +0530

    Time: 4 ms (73.35%) | Memory: 45.5 MB (30.12%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,6 +1,32 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans=new int[nums1.length];
         HashMap<Integer, Integer> map=new HashMap<>();
+
+        Stack<Integer> stk=new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--)
+        {
+            while(!stk.isEmpty() && stk.peek()<=nums2[i]) stk.pop();
+            if(stk.isEmpty()) map.put(nums2[i], -1);
+            else map.put(nums2[i], stk.peek());
+            stk.push(nums2[i]);
+        }
+
+        for(int i=0;i<nums1.length;i++)
+        {
+            ans[i]=map.get(nums1[i]);
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+    /** HashMap<Integer, Integer> map=new HashMap<>();
         Stack<Integer> stk=new Stack<>();
         int []ans=new int[nums1.length];
         for(int i=nums2.length-1;i>=0;i--){
@@ -21,9 +47,12 @@ class Solution {
         for(int i=0;i<nums1.length;i++){
             ans[i]=map.get(nums1[i]);
         }
-        return ans;
-    }
-}
+        return ans; */
+
+
+
+
+
  /**  int[] ans = new int[nums1.length];
         Arrays.fill(ans, -1);
         //int index = 0;
