68d10729374b08dbb5beea520ee95f765143c022
YasharthChaudhary761 <yasharthchaudhary766@gmail.com>
2026-03-26 22:22:30 +0530

    Time: 6 ms (69.42%) | Memory: 45.8 MB (64.08%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -7,14 +7,14 @@ class Solution {
         while(r<n)
         {
             int len=0;
-            if(map.containsKey(s.charAt(r)))
+            if(map.containsKey(s.charAt(r)))  // ye chewck kr rha hai ki map me rth index pe character hai ya mii..agar hai to next condition check hogi
             {
-                if(map.get(s.charAt(r))>=l)
+                if(map.get(s.charAt(r))>=l) // ye map me jo character store hua hai uska index check krr rha hai ki rth index l wale pointer se bada hai ya nii..agar bada hai to l wala pointer apne jagah se ek index aage badh jayega
                 {
-                    l=map.get(s.charAt(r))+1;
+                    l=map.get(s.charAt(r))+1; // yaha pe actually me left pointer ek increment ho rha hai
                 }
             }
-            len=r-l+1;
+            len=r-l+1; //yaha parr substring ki length calculate ho rhi hai 
             max=Math.max(max, len);
             map.put(s.charAt(r),r);
             r++;
